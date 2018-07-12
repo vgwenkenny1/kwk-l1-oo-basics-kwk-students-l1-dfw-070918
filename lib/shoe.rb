216183ac0ad8c68a2fb2b4cc9c8aea76b5b95b36
vgwenkenny1 <vgwenkenny1@gmail.com>
2018-07-12 20:44:09 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,6 @@
 class Shoe
   def initialize(brand, color, size, material, condition)
-    @brand = brand
-    @color = color
-    @size = size
-    @material = material
-    @condition = condition
-  end
+
 
   
   shoe_one = shoe.new("Adidas")
